9290f808d0d3b072cbf55473920b44b71937da52
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-12-02 14:24:30 +0700

    init server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,50 @@
-/**
- * Created by tamazlykar_dv on 02.12.16.
+/*TODO
+1. Make other files unavalible with path
+2. Write who connecting who disconnecting chat
+3. Create config and set port there
  */
+'use strict';
+
+let server = require('http').createServer();
+let WebSocketServer = require('ws').Server;
+let webSocketServer = new WebSocketServer({ server });
+let express = require('express');
+let app = express();
+const port = 4800;
+
+let clients = {};
+
+
+app.use(express.static('prod'));
+
+webSocketServer.on('connection', (ws) => {
+  let id = ID();
+  clients[id] = ws;
+  console.log(`new connection ${id}`);
+
+  ws.on('message', (message) => {
+    for (let key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log(`connection closed ${id}`);
+    delete clients[id];
+  })
+});
+
+/**
+ * @return {string}
+ */
+function ID() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+
+server.on(`request`, app);
+server.listen(port, () => {
+  console.log(`Listening on ${server.address().port}`);
+});
+
+
